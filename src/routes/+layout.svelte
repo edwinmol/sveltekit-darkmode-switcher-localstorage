020230b6b45a8435be1bd1ffdf82e831ff2b575b
Edwin Mol <edwin@siteware.be>
2023-12-27 10:31:47 +0100

    Update readme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,28 +5,44 @@
 	
 </script>
 
-<div class="app-bar">
-	<ThemeSwitcher />
-</div>
+<nav class="app-bar">
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/about">About</a>
+		</li>
+		<li>
+			<ThemeSwitcher />
+		</li>
+	</ul>
+</nav>	
+	
 <main class="content">
 	<slot />
 </main>
 
 <style>
-	.app-bar {
+	.app-bar ul {
+		margin: 0;
+		list-style-type: none;
 		display: flex;
 		flex-direction: row;
-		justify-content: space-between;
+		justify-content: center;
 		align-items: center;
 		padding: 0 16px;
 		height: 48px;
+		gap: 1.5rem;
 		background-color: var(--app-bar-bkg-color);
+	}
+	.app-bar ul li a {
+		text-decoration: underline;
+		color: var(--text-color);
 	}
 	.content {
 		padding: 1rem;
 		max-width: 36rem;
 		margin: 0 auto;
 	}
-	/* TODO: set the font family and use a variable 
-    font: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_fonts/Variable_fonts_guide */
 </style>
